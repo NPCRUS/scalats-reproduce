1cdce26cfcdd22e32a6f7be75726534807b17e5d
NPCRUS <npcrus@gmail.com>
2023-08-01 08:51:31 +0200

    generate examples

--- api/src/main/scala/Main.scala
+++ api/src/main/scala/Main.scala
@@ -1,9 +1,5 @@
 import models.{ADT, FruitType, GenericCaseClass, SimpleCaseClass}
 
-object Main extends App {
-
-}
-
 case class EnumTest(a: String, b: FruitType.FruitType)
 
 case class SimpleCaseClassTest(a: String, b: SimpleCaseClass)
